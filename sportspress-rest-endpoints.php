97787fe25c8d888ac8402e3307601bc91899c4a0
mmarcec007 <mark.marcec@gmail.com>
2021-07-12 20:08:49 +0200

    update sportspress-rest-endpoints.php plugin

--- sportspress-rest-endpoints.php
+++ sportspress-rest-endpoints.php
@@ -10,14 +10,16 @@ Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
 
+include_once ('includes/functions.php');
+
 add_action( 'rest_api_init', function () {
-    require plugin_dir_path( __FILE__ ) . 'includes/VenuesController.php';
+    require plugin_dir_path( __FILE__ ) . 'includes/controllers/VenuesController.php';
     $controller = new VenuesController();
     $controller->register_routes();
 } );
 
 add_action( 'rest_api_init', function () {
-    require plugin_dir_path( __FILE__ ) . 'includes/EventsController.php';
+    require plugin_dir_path( __FILE__ ) . 'includes/controllers/EventsController.php';
     $controller = new EventsController();
     $controller->register_routes();
 } );
